257cc42da43bcc8c06b5c238dcbe2f941196dfe8
acatasic <96747833+acatasic@users.noreply.github.com>
2022-03-16 00:02:34 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using zoo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace zoo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+          
+          
+            services.AddDbContext<zooContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("AviokompanijaCS"));
+            });
+
+            services.AddCors(p => 
+            {
+                p.AddPolicy("CORS", builder => 
+                {
+                    builder.AllowAnyMethod()
+                           .AllowAnyOrigin()
+                           .AllowAnyHeader();
+                });
+            });
+
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+             ;
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("CORS");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
